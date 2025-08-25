61d7600af7c1fa2ce1b99d947d05f61106f17509
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-08-25 12:51:45 +0530

    Attribution changed

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@ if (navigator.geolocation) {
 const map = L.map("map").setView([0, 0], 16); //(coordinates,zoom)
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "OpenStreepMap",
+    attribution: "OnDaGo",
 }).addTo(map);
 
 const markers = {};
